09b3969383b57b7a7d2a7d05928d84bb6d9fd808
Attila Domokos <adomokos@gmail.com>
2017-08-21 20:44:42 -0500

    No need for those parens

--- test/CrudSqliteSpec.hs
+++ test/CrudSqliteSpec.hs
@@ -10,9 +10,9 @@ spec :: Spec
 spec = do
     describe "finds records in the DB" $ do
         it "finds a band by ID" $ do
-            (Just band) <- findBand 1
-            (name band) `shouldBe` "The Beatles"
+            Just band <- findBand 1
+            name band `shouldBe` "The Beatles"
         it "finds a band by name" $ do
-            (Just band) <- findByName "The Beatles"
-            (formedYear band) `shouldBe` 1960
-            (genre band) `shouldBe` Just "Rock"
+            Just band <- findByName "The Beatles"
+            formedYear band `shouldBe` 1960
+            genre band `shouldBe` Just "Rock"
